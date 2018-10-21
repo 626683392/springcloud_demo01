fe391a5a6cf9d2d9330389690935f87567e91bf0
tao <626683392@qq.com>
2018-10-21 18:25:24 +0800

    增加测试

--- member_provider/src/main/java/com/taoai77/controller/MemberController.java
+++ member_provider/src/main/java/com/taoai77/controller/MemberController.java
@@ -24,7 +24,6 @@ public class MemberController {
                 "server.port:"+port);
     }
 
-
     @RequestMapping("/getOtherAllMember")
     public List<String> getOtherAllMember(){
         log.info("正在延迟"+port+"端口号的接口...");
